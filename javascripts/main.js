4198b5a1812ded568a6ec0c4cb20fed9e98a88aa
ellisthomas <ellisthomas80@gmail.com>
2017-04-09 20:31:03 -0500

    completed zoe project

---
+++ javascripts/main.js
@@ -0,0 +1,9 @@
+console.log("Brielle");
+
+// $(document).on("click", "#btn", function() {
+// 	alert("button cliked");
+// });
+
+$(document).on("click", "#btn", function() {
+	$("#zoeText").text("To succeed in life, you need three things: a wishbone, a backbone and a funny bone.");
+});
